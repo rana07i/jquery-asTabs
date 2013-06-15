be490e1ff8e07eecf25fc43d21a076548e0aa87a
Wowhoo <fish@fish-virtual-machine.(none)>
2013-06-15 11:02:50 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('jquery-tabs.jquery.json'),
+    pkg: grunt.file.readJSON('package.json'),
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
@@ -71,6 +71,34 @@ module.exports = function(grunt) {
         tasks: ['jshint:test', 'qunit']
       },
     },
+    jsbeautifier: {
+        files: ["Gruntfile.js", "src/**/*.js"],
+        options: {
+            "indent_size": 4,
+            "indent_char": " ",
+            "indent_level": 0,
+            "indent_with_tabs": true,
+            "preserve_newlines": true,
+            "max_preserve_newlines": 10,
+            "jslint_happy": false,
+            "brace_style": "collapse",
+            "keep_array_indentation": false,
+            "keep_function_indentation": false,
+            "space_before_conditional": true,
+            "eval_code": false,
+            "indent_case": false,
+            "unescape_strings": false
+        }
+    },
+    recess: {
+        core: {
+            src: ["less/**/*.less"],
+            dest: 'demo/css/tabs.css',
+            options: {
+                compile: true
+            }
+        },
+    }
   });
 
   // These plugins provide necessary tasks.
@@ -81,7 +109,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  grunt.loadNpmTasks('grunt-jsbeautifier');
+  grunt.loadNpmTasks('grunt-recess');
+
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
+
+  grunt.registerTask('js', ['jsbeautifier', 'jshint']);
+  grunt.registerTask('css', ['recess']);
 
 };
--- src/jquery-tabs.js
+++ src/jquery-tabs.js
@@ -6,33 +6,178 @@
  * Licensed under the MIT license.
  */
 
-(function($) {
+;(function(window, document, $, undefined) {
+    "use strict";
 
-  // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
-    });
-  };
+    // Constructor
+    var Tabs = $.Tabs = function(element, options) {
+        // Attach element to the 'this' keyword
+        this.element = element;
+        this.$element = $(element);
 
-  // Static method.
-  $.awesome = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
+        // options
+        var meta_data = [];
+        $.each(this.$element.data(), function(k, v) {
+            var re = new RegExp("^tabs", "i");
+            if (re.test(k)) {
+                meta_data[k.toLowerCase().replace(re, '')] = v;
+            }
+        });
+        this.options = $.extend(true, {}, Tabs.defaults, options, meta_data);
 
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
+        // Namespacing
+        var namespace = this.options.namespace;
 
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
+        // Class
+        this.classes = {};
+        this.classes.activeTab = this.options.namespace + '-active';
+        this.classes.activePanes = this.options.namespace + '-active';
 
-}(jQuery));
+        this.$tabs = this.$element.find( this.options.tabSelector );
+        this.$panes = this.$element.find( this.options.paneSelector );
+        this.$element.addClass(this.options.skin);
+
+        var self = this;
+        $.extend(self, {
+            init: function() {
+                self.current = this.options.initialIndex;
+                self.active(self.current);
+
+                // Bind logic
+                self.$tabs.on(this.options.event, function(e, data) {
+                    e.stopPropagation();
+
+                    var index = $(e.target).index();
+
+                    self.active(index);
+                });
+            },
+            another: function(){
+            
+            
+            },
+
+        });
+
+        self.init();
+    };
+
+
+    // Default options for the plugin as a simple object
+    Tabs.defaults = {
+        namespace: 'tabs',
+        tabSelector: '.tabs > li',
+        paneSelector: '.panes > div',
+        skin: null,
+        initialIndex: 0,
+
+        // autoplay: 0,
+        // speed : 300,
+        // time : 400,
+
+        //effect: 'default';//fade slide ajax
+
+        // pauseOnHover: 0, 
+
+        event : 'click'
+    };
+
+    Tabs.prototype = {
+        constructor: Tabs,
+        // This is a public function that users can call
+        // Prototype methods are shared across all elements
+        active: function(index){
+            // this.$panes.eq(index).css('display','block').siblings().css('display','none');
+            this.current = index;
+
+            this.$tabs.eq(index).addClass(this.classes.activeTab).siblings().removeClass(this.classes.activeTab);
+
+            this.$panes.eq(index).addClass(this.classes.activePanes).siblings().removeClass(this.classes.activePanes);
+        },
+
+        getTabs: function(){
+            return this.$tabs;
+        },
+
+        getPanes: function(){
+            return this.$panes;
+        },
+
+        getCurrentPane: function() {
+            return this.$panes.eq(this.current);
+        },
+
+        getCurrentTab: function() {
+           return this.$tabs.eq(this.current);
+        },
+
+        getIndex: function() {
+            return this.current;
+        }, 
+
+        next: function() {
+            var len = this.$tabs.length,
+                current = this.current;
+            if (current < len-1) {
+                current++; 
+            } 
+            else {
+                current = 0;
+            }
+
+            // (current < len-1) ? current++ : current = 0;
+
+            this.active(current);
+
+        },
+
+        prev: function() {
+            var len = this.$tabs.length,
+                current = this.current;
+            if (current == 0 ) {
+                current = Math.abs( 1 - len );
+            } 
+            else {
+                current = current - 1;
+            }
+
+            this.active(current);
+        },
+
+        destroy: function() {
+            // console.log(this.$element)
+            this.$element.remove();
+            // this.$tabs.off(this.options.event).removeClass(this.classes.activeTab);
+            // this.$panes.eq(this.current).removeClass(this.classes.activePanes); 
+            // return this;
+        }
+    };
+
+    // Collection method.
+    $.fn.tabs = function(options) {
+        if (typeof options === 'string') {
+            var method = options;
+            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+
+            if(/^(getTabs|getPanes|getCurrentPane|getCurrentTab|getIndex)$/.test(method)){
+                var api = this.first().data('tabs');
+                if (api && typeof api[method] === 'function') {
+                    return api[method].apply(api, method_arguments);
+                }
+            } else {
+                return this.each(function() {
+                    var api = $.data(this, 'tabs');
+                    if (api && typeof api[method] === 'function') {
+                        api[method].apply(api, method_arguments);
+                    }
+                });
+            }
+        } else {
+            return this.each(function() {
+                if (!$.data(this, 'tabs')) {
+                    $.data(this, 'tabs', new Tabs(this, options));
+                }
+            });
+        }
+    };
+}(window, document, jQuery));
