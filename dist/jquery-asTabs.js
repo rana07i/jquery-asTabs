a3ec595077a62b5634258f9562a0bc70d0d2a0fe
amazingSurge <amazingSurge@yahoo.com>
2014-09-04 05:42:29 -0700

    update

--- dist/jquery-asTabs.js
+++ dist/jquery-asTabs.js
@@ -1,4 +1,4 @@
-/*! jQuery asTabs - v0.3.1 - 2014-08-02
+/*! jQuery asTabs - v0.3.1 - 2014-09-04
 * https://github.com/amazingSurge/jquery-asTabs
 * Copyright (c) 2014 amazingSurge; Licensed GPL */
 (function($, document, window, undefined) {
@@ -321,12 +321,11 @@
     $.fn[pluginName] = function(options) {
         if (typeof options === 'string') {
             var method = options;
-            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : [];
 
             if (/^\_/.test(method)) {
                 return false;
             } else if (/^(getTabs|getPanes|getCurrentPane|getCurrentTab|getIndex|getSize)$/.test(method)) {
-
                 var api = this.first().data(pluginName);
                 if (api && typeof api[method] === 'function') {
                     return api[method].apply(api, method_arguments);
--- src/jquery-asTabs.js
+++ src/jquery-asTabs.js
@@ -325,12 +325,11 @@
     $.fn[pluginName] = function(options) {
         if (typeof options === 'string') {
             var method = options;
-            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : [];
 
             if (/^\_/.test(method)) {
                 return false;
             } else if (/^(getTabs|getPanes|getCurrentPane|getCurrentTab|getIndex|getSize)$/.test(method)) {
-
                 var api = this.first().data(pluginName);
                 if (api && typeof api[method] === 'function') {
                     return api[method].apply(api, method_arguments);
